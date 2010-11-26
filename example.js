ad28542d0fb6b54b073c96bbe46e1774e3fa0b9b
Alexander Wolfe <thewolfe@alexander-wolfes-imac-3.local>
2010-11-26 13:41:54 -0800

    First working example of iStorage class

--- example.js
+++ example.js
@@ -1,9 +1,36 @@
-var Example = new Class({
-	initialize: function() {
-		
-		
-		
-	}
-	
-	
-}); 
+document.addEvent('domready', function(){
+    var iStore = new iStorage(); 
+    
+    
+    function storeValues() {
+        //STORE KEYS WITH DIFFERENT DATA TYPES
+        iStore.set('mynum', 125); 
+        iStore.set('mybool', true); 
+        iStore.set('mystring', 'hello world'); 
+        iStore.set('list', ['dog', 'cat', 'sheep']); 
+        iStore.set('myobj',  {
+            'color': 'red',
+            'shape': 'circle',
+            'height': 100, 
+            'width': 200
+        }); 
+    }
+    
+    $('save-data').addEvent('click', storeValues); 
+    $('clear').addEvent('click', iStore.clear); 
+    
+    if(iStore.get('mystring')) {
+        $$('h2').set('html', iStore.get('mystring'));
+    }
+    
+    if(iStore.get('list')) {
+        var list = $('list');
+        var listItems = iStore.get('list'); 
+        
+        listItems.each(function(item){
+            list.adopt(new Element('li', {
+                'html': item
+            })); 
+        });
+    }
+});
--- storage.js
+++ storage.js
@@ -1,8 +1,77 @@
-var MyStorage = new Class({
-	initialize: function() {
-		
-		
-		
-	}
+//HTML5 LOCAL STORAGE
+//
+//NAME: iStorage
+//AUTHOR: Alexander L. Wolfe
+//DEPENDANICES: MooTools Core https://ajax.googleapis.com/ajax/libs/mootools/1.3.0/mootools-yui-compressed.js
+//COMPATIBILITY: IE8+, FF 3.5+, Safari 4.0+, Chrome 4.0+, Opera 10.5+, iPhone 1.0+, Android 2.0+ 
+
+//NATIVE Object
+//window.localStorage 
+//
+//NATIVE METHODS
+//setItem(key, item)
+//getItem(key)
+//removeItem(key)
+//clear()
+//key()
+//length
+//
+//NATIVE EVENTS
+//'storage' (for all modern browsers)
+//window.event (for ie)
+//
+//NATIVE STORAGE SIZE
+//5mb
+//QUTOA_EXCEEDED_ERR 
+
+
+var iStorage = new Class({
+    Implements:[Options, Events], 
+    options: {
+        'prefix': 'istore_'
+    }, 
+    
+	initialize: function(options) {
+	   this.setOptions(options); 
+		if(this.supportsStorage) {
+		  
+		} else {
+		  return false;
+		}
+	}, 
 	
+	clear: function() {
+	   localStorage.clear(); 
+	}, 
+	
+	count: function() {
+	   return localStorage.length; 
+	},
+	
+	get: function(key) {
+	   var skey = this.options.prefix + key; 
+	   var item = localStorage.getItem(skey); 
+	   if(item) {
+	       var data = JSON.decode(item); 
+	       this.fireEvent('yes_item');
+	       return data.data;  
+	   } else {
+	       this.fireEvent('no_item');
+	       return false; 
+	   }
+	}, 
+	
+	remove: function(key) {
+	   localStorage.removeItem(this.options.prefix + key); 
+	}, 
+	
+	set: function(key, value) {
+        var data = JSON.encode({type: value.constructor,data: value}); 	   
+        localStorage.setItem(this.options.prefix + key, data); 
+        this.fireEvent('set_item');
+	}, 
+	
+	supportsStorage: function() {
+	   return ('localStorage' in window) && window['localStorage'] !== null;
+	}
 }); 
